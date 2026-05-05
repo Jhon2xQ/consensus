9ddeb752c8d00d303624cbb58e0e7b92a90c7b83
Jhon2xQ <jhonq2512@gmail.com>
2026-05-05 14:46:11 -0500

    fist commit

---
+++ src/main/java/com/carmenio/consensus/controllers/TestController.java
@@ -0,0 +1,13 @@
+package com.carmenio.consensus.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+  
+  @GetMapping("/hello")
+  public String getString() {
+    return "Hello world";
+  }
+}
